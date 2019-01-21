296a57fb97775be6149cd88c7112c666b2f58d9b
oscarandyuli <46876296+oscarandyuli@users.noreply.github.com>
2019-01-20 18:06:04 -0600

    uno

---
+++ precache-manifest.25d86146e963f2b8a9450cb82630828a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d152a6594f4ea094e90",
+    "url": "/static/css/main.61edf6a8.chunk.css"
+  },
+  {
+    "revision": "7d152a6594f4ea094e90",
+    "url": "/static/js/main.7d152a65.chunk.js"
+  },
+  {
+    "revision": "1a54d30b1d4ee2c6384e",
+    "url": "/static/js/1.1a54d30b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18bbac08b6ed49895a1a7416beca0cdf",
+    "url": "/static/media/flower.18bbac08.png"
+  },
+  {
+    "revision": "07a06542fef2592d07637da0dcf25050",
+    "url": "/static/media/rings.07a06542.png"
+  },
+  {
+    "revision": "04e2b44f728e7cc0308a4bd08f450f23",
+    "url": "/static/media/giftFlower.04e2b44f.png"
+  },
+  {
+    "revision": "e00acf4e09cb535d22e600df7b046b04",
+    "url": "/static/media/fondo.e00acf4e.png"
+  },
+  {
+    "revision": "0cdf99617f18215261ec1ec4af9c72e4",
+    "url": "/index.html"
+  }
+];
